ca00e7f2ced3d523f6766b5b2a80513a1f179f8a
Patryk Kisielewski <pkisielewski@simplito.com>
2025-03-19 14:28:01 +0100

    docs: Fix documentation comment
    Refs: PECS-14

--- PrivMX.Endpoint/Core/Models/Error.cs
+++ PrivMX.Endpoint/Core/Models/Error.cs
@@ -17,7 +17,7 @@ namespace PrivMX.Endpoint.Core.Models
     public class Error
     {
         /// <summary>
-        /// Id of the native exception.
+        /// ID of the native exception.
         /// </summary>
         public long Code { get; set; }
 
