ebfc57a396741c1a55aecb59baabcda924a8a05c
Klaudia O <62263707+Loxx-KO@users.noreply.github.com>
2026-01-21 10:07:00 +0100

    StreamApi wip

--- PrivMX.Endpoint/Stream/IStreamApi.cs
+++ PrivMX.Endpoint/Stream/IStreamApi.cs
@@ -9,11 +9,40 @@
 // limitations under the License.
 //
 
+using System.Collections.Generic;
+using PrivMX.Endpoint.Core.Models;
+using PrivMX.Endpoint.Stream.Models;
+using PrivMX.Endpoint.Stream.Models.Events;
+
 namespace PrivMX.Endpoint.Stream
 {
     public interface IStreamApi
     {
-        string GetTurnCredentials();
-        
+        List<TurnCredentials> GetTurnCredentials();
+        string CreateStreamRoom(string contextId, List<UserWithPubKey> users, List<UserWithPubKey> managers, 
+            byte[] publicMeta, byte[] privateMeta, ContainerPolicy? policies = null);
+        void UpdateStreamRoom(string streamRoomId, List<UserWithPubKey> users, List<UserWithPubKey> managers, 
+            byte[] publicMeta, byte[] privateMeta, long version, bool force, bool forceGenerateNewKey, 
+            ContainerPolicy? policies = null);
+        PagingList<StreamRoom> ListStreamRooms(string contextId, PagingQuery pagingQuery);
+        StreamRoom GetStreamRoom(string streamRoomId);
+        void DeleteStreamRoom(string streamRoomId);
+        List<StreamInfo> ListStreams(string streamRoomId);
+        void JoinStreamRoom(string streamRoomId, WebRTCInterface webRtc);
+        void LeaveStreamRoom(string streamRoomId);
+        long CreateStream(string streamRoomId);
+        StreamPublishResult PublishStream(long streamHandle);
+        StreamPublishResult UpdateStream(long streamHandle);
+        void UnPublishStream(long streamHandle);
+        void SubscribeToRemoteStreams(string streamRoomId, List<StreamSubscription> subscriptions, Settings options);
+        void ModifyRemoteStreamsSubscriptions(string streamRoomId, List<StreamSubscription> subscriptionsToAdd, 
+            List<StreamSubscription> subscriptionsToRemove, Settings options);
+        void UnsubscribeFromRemoteStreams(string streamRoomId, List<StreamSubscription> subscriptionsToRemove);
+        void Trickle(long sessionId, string candidateAsJson);
+        void AcceptOfferOnReconfigure(long sessionId, SdpWithTypeModel sdp);
+        List<string> SubscribeFor(List<string> subscriptionQueries);
+        void UnsubscribeFrom(List<string> subscriptionIds);
+        string BuildSubscriptionQuery(EventType eventType, EventSelectorType selectorType, string selectorId);
+        void KeyManagement(string streamRoomId, bool disable);
     }
 }
